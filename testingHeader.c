317bda2ae118949f98456600e329c971795be35d
Chandan <imjunior471@gmail.com>
2020-11-30 00:02:46 +0530

    initial dsalgo

---
+++ testingHeader.c
@@ -0,0 +1,8 @@
+#include<conio.h>
+#include<stdio.h>
+#include "myOwnHeader.h"//you have to put your customized header file name in (" ")quotes.
+
+int main(){
+     printf("Hello Header file..");
+     myfunc();
+}
